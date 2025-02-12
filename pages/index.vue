f9c96071f82ff6b957ec51e38975c1dff539e58f
nanun2557 <nanun2557@gmail.com>
2025-02-12 23:52:36 +0700

    try use icon components

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,8 @@
         <Alert />
         <Profile />
         <div>Main page</div>
+        <IconsAirplane />
         <img src="@/assets/img/girlanddog.png" alt="girl and dog">
+        <img src="/tvandfan.png" alt="tv and fan">
     </div>
 </template>
