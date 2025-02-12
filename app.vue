de366433dd951d628e2a77029c292a258be5aab7
nanun2557 <nanun2557@gmail.com>
2025-02-12 18:27:40 +0700

    example log nuxt object

--- app.vue
+++ app.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
-    <!-- <NuxtRouteAnnouncer /> -->
-    <!-- <NuxtWelcome /> -->
-     <div>Hello World</div>
-     <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-     </div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
