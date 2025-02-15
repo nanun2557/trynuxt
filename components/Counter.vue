861576cea1e13b8650729175bd7bfaec8ca7fdf1
Osacn nanun Junjaroen <osacn_nanun.junjaroen@krungthai.com>
2025-02-15 15:16:36 +0700

    add example useState for stage management

---
+++ components/Counter.vue
@@ -0,0 +1,11 @@
+<script setup>
+const counter = useCounter();
+
+</script>
+<template>
+    <div class="counterComponent">
+        Counter : {{ counter }}
+        <button class="p-1 bg-white text-black border border-gray-300 rounded-md cursor-pointer" @click="counter++">+</button>
+        <button class="p-1 bg-white text-black border border-gray-300 rounded-md cursor-pointer" @click="counter--">-</button>
+    </div>
+</template>
